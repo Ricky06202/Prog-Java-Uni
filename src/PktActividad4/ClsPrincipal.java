814696dfc477dd34be268017922e0fc76c88e867
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2022-05-09 21:11:18 -0500

    probamos unas cosas

--- src/PktActividad4/ClsPrincipal.java
+++ src/PktActividad4/ClsPrincipal.java
@@ -2,6 +2,6 @@ package PktActividad4;
 public class ClsPrincipal {
     public static void main(String[] args) {
         ClsSecundaria objDeClsSecundaria = new ClsSecundaria();
-        
+        objDeClsSecundaria.mtdCalculoDeLaEdadExacta();
     }
 }
